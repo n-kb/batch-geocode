0d414bede05b062e5e472cccf09f0a6a59dc2d37
Nicolas Kayser-Bril <hi@nkb.fr>
2018-06-13 20:07:40 +0200

    changed things

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
 
-var app = express()
-app.use(serveStatic(path.join(__dirname)))
+let app = express();
+app.use(serveStatic(__dirname));
 
-var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started ' + port)
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('Listening on port ' + port)
+});
