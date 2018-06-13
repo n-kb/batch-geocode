9cf599c57da9a814cbdaf8366a4ff437447179b7
Nicolas Kayser-Bril <hi@nkb.fr>
2018-06-13 19:55:21 +0200

    changed things

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
-// Imports.
-const express = require('express')
-const app = express()
-
-// Express server.
-const staticFileMiddleware = express.static(__dirname)
-app.use(staticFileMiddleware)
-app.use(history({
- disableDotRule: true,
- verbose: true
-}))
-app.use(staticFileMiddleware)
-
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
 var port = process.env.PORT || 5002;
 
-app.listen(port, () => {
- console.log('App listening on port ${5002}!')
-})
+app = express();
+
+app.use(serveStatic(__dirname));
+
+app.listen(port);
