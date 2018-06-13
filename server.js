430dd0e473f5434878a04c53f71fd1d608058cb6
Nicolas Kayser-Bril <hi@nkb.fr>
2018-06-13 19:58:31 +0200

    changed things

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path')
 var serveStatic = require('serve-static')
 
 var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(serveStatic(path.join(__dirname)))
 
 var port = process.env.PORT || 5000
 app.listen(port)
